57857d49296253749b6f2efb728ec6dfbcea259b
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2024-12-02 19:37:51 -0300

    feat: add routing for MainNewsComponent with redirect to inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'inicio',
+    pathMatch: 'full',
+  },
+  {
+    path: 'inicio',
+    loadComponent: () =>
+      import('./components/main-news/main-news.component').then(
+        (c) => c.MainNewsComponent
+      ),
+  },
+];
