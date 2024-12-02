8e8f372244616c48cd69b0eb66556e08ec46ab13
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2024-12-02 19:37:34 -0300

    feat: implement NewsService to get news articles from API

---
+++ src/app/services/news.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { INews } from '../interfaces/news.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NewsService {
+  http = inject(HttpClient);
+
+  private readonly API = 'https://daily-news-api-tau.vercel.app';
+
+  getMainNews(): Observable<INews[]> {
+    return this.http.get<INews[]>(`${this.API}/main-news`);
+  }
+
+  getVariedNews(): Observable<INews[]> {
+    return this.http.get<INews[]>(`${this.API}/varied-news`);
+  }
+}
