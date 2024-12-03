c594f4dcd97a881cd212e91da3dc6a7020e1fcf8
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2024-12-03 17:39:01 -0300

    feat: implement carousel for main news with Bootstrap styling and remove unused SCSS

--- src/app/components/main-news/main-news.component.ts
+++ src/app/components/main-news/main-news.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { INews } from '../../interfaces/news.interface';
 import { NewsService } from '../../services/news.service';
 
 @Component({
   selector: 'app-main-news',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './main-news.component.html',
   styleUrl: './main-news.component.scss',
 })
@@ -12,6 +13,7 @@ export class MainNewsComponent implements OnInit {
   newsService = inject(NewsService);
 
   mainNews = signal<INews[] | null>(null);
+  isFirst = signal<boolean>(true);
 
   ngOnInit(): void {
     this.newsService.getMainNews().subscribe((response) => {
