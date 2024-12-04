5717ee795f4f143c4b411bfa6c17562f6e68c198
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2024-12-04 17:22:46 -0300

    feat: enhance main news component with recent news integration and styling adjustments

--- src/app/components/main-news/main-news.component.ts
+++ src/app/components/main-news/main-news.component.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { INews } from '../../interfaces/news.interface';
 import { NewsService } from '../../services/news.service';
+import { RecentNewsComponent } from '../recent-news/recent-news.component';
 
 @Component({
   selector: 'app-main-news',
-  imports: [CommonModule],
+  imports: [CommonModule, RecentNewsComponent],
   templateUrl: './main-news.component.html',
   styleUrl: './main-news.component.scss',
 })
