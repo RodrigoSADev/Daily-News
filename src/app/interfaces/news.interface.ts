1e830d294ccf11cc38dff5812367a1acbe98ed86
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2024-12-20 15:26:26 -0300

    feat: refactor news interfaces to improve structure and add new types

--- src/app/interfaces/news.interface.ts
+++ src/app/interfaces/news.interface.ts
@@ -1,11 +1,34 @@
-export interface INews {
+export interface IMainNew {
+  id: number;
+  title: string;
+  subtitle: string[];
+  content: string;
+  fullContent: string;
+  date: string;
+  publishedAt: string;
+  author: string;
+  category: string;
+  image: string[];
+  alt: string;
+}
+
+export interface IRecentNew {
   id: number;
   title: string;
   subtitle: string;
-  content?: string;
+  fullContent: string;
+  publishedAt: string;
+  author: string;
+  image: string;
+  alt: string;
+}
+
+export interface IVariedNew {
+  id: number;
+  title: string;
+  subtitle: string;
   fullContent: string;
   date: string;
-  publishedAt?: string;
   author: string;
   category: string;
   image: string;
