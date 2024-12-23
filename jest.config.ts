dd9eccfc09c607ec7c3b593bc342ec22b03f907a
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2024-12-23 17:54:59 -0300

    fix: update Jest and Playwright configurations to ignore Playwright tests and change test directory

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'jest';
 const jestConfig: Config = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testPathIgnorePatterns: ['<rootDir>/playwright/'],
 };
 
 export default jestConfig;
